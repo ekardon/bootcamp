db2d3ee36347cf176f77208fdf17ea657832c609
mbilal <mbilalkilicoglu@gmail.com>
2022-01-09 22:38:56 +0300

    - internal imports corrected

--- ecommerce/customers/admin.py
+++ ecommerce/customers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .models import Customer
+from customers.models import Customer
 
 
 @admin.register(Customer)
--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-from .managers import CustomerManager
+from customers.managers import CustomerManager
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
